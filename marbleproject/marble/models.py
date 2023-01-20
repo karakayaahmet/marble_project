197e531f499387c4ec2ba217522ac3b2a9777862
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-20 18:27:14 +0300

    oldu gibi.

--- marbleproject/marble/models.py
+++ marbleproject/marble/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from PIL import Image
-from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.utils import img_to_array, load_img
 from keras.preprocessing import image
 from tensorflow.python import ops
 from keras.models import load_model
@@ -25,6 +25,11 @@ class Resimler(models.Model):
     title = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     average_color = models.CharField(max_length=20, blank=True)
+    crack = models.CharField(max_length=4, blank=True)
+    dot = models.CharField(max_length=4, blank=True)
+    good = models.CharField(max_length=4, blank=True)
+    joint = models.CharField(max_length=4, blank=True)
+    kalite_turu = models.CharField(max_length=5, blank=True)
 
     class Meta:
         get_latest_by = 'created_at'
@@ -32,14 +37,15 @@ class Resimler(models.Model):
 
     def save(self, *args, **kwargs):
         img = Image.open(self.image)
-        test_img = img.resize((224,224))
-        test_img = img_to_array(test_img)
-        test_img = np.expand_dims(test_img, axis = 0)
+        
 
         classes_tur = ["AageanRose", "AfyonBal", "AfyonBeyaz", "AfyonBlack", "AfyonGrey", "AfyonSeker", "Bejmermer", "Blue", "Capuchino", "Diyabaz", "DolceVita", "EkvatorPijama", "ElazigVisne", "GoldGalaxy", "GulKurusu", "KaplanPostu", "Karacabeysiyah", "Konglomera", "KristalEmprador", "Leylakmermer", "MediBlack", "OliviaMarble", "Oniks", "RainGrey", "Traverten"]
 
 
         try:
+            test_img = img.resize((224,224))
+            test_img = img_to_array(test_img)
+            test_img = np.expand_dims(test_img, axis = 0)
             interpreter = tf.lite.Interpreter(model_path="yeni-tur_model.tflite")
             interpreter.allocate_tensors()
             input_details = interpreter.get_input_details()
@@ -95,22 +101,130 @@ class Resimler(models.Model):
             #     pred = np.argmax(model.predict(test_img))
             #     self.title = str(classes_tur[pred])
 
+
+            
+
+
+
         except:
             self.title = "Sınıflandırma Başarısız"
             self.average_color = "Renk Kodu Bulunamadı"
 
+        try:
+            # Çatlak türü analizi:
+            #image load with numpy
+            img = Image.open(self.image)
+            test_img = img.resize((256,256))
+            test_img = img_to_array(test_img)
+            test_img = np.expand_dims(test_img, axis = 0)
+
+            """
+            import cv2
+            #image load with opencv
+            img = cv2.imread("deneme/visne.png")
+            img = cv2.resize(img, (224,224))
+            img = np.array(img, dtype="float32")
+            img = np.reshape(img, (1,224,224,3))
+            """
+
+            #Modelin yüklenmesi ve giriş çıkış tensorlerin hazırlanması
+            interpreter = tf.lite.Interpreter(model_path="yepyeni_catlak.tflite")
+            interpreter.allocate_tensors()
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+
+
+
+            #Giriş tensorü olarak diziye dönüştürülmüş imgenin verilmesi ve prediction işlemi
+            interpreter.set_tensor(input_details[0]['index'],test_img)
+            interpreter.invoke()
+            predictions = tf.math.softmax(interpreter.get_tensor(output_details[0]['index']))
+
+
+            print(predictions)
+
+
+
+            #olasılıklar arasından en büyüğünün seçilmesi ve tür eşleştirilmesi
+            max_index = np.argmax(predictions)
+
+            sonuclar = []
+
+            for i in predictions:
+                sonuclar.append(i.numpy())
+
+            print(classes[max_index])
+
+            crack1 = "{:.2f}".format(sonuclar[0][0]*100)
+            dot1 = "{:.2f}".format(sonuclar[0][1]*100)
+            good1 = "{:.2f}".format(sonuclar[0][2]*100)
+            joint1 = "{:.2f}".format(sonuclar[0][3]*100)
+            # self.crack = str(sonuclar[0][0]*100)
+            # self.dot = str(sonuclar[0][1]*100)
+            # self.good = str(sonuclar[0][2]*100)
+            # self.joint = str(sonuclar[0][3]*100)
+
+            self.crack = crack1
+            self.dot = dot1
+            self.good = good1
+            self.joint = joint1
+
+
+        except:
+
+            self.crack = "--"
+            self.dot = "--"
+            self.good = "--"
+            self.joint = "--"
+
+        try:
+            # Çatlak türü analizi:
+            #image load with numpy
+            img = Image.open(self.image)
+            test_img = img.resize((256,256))
+            test_img = img_to_array(test_img)
+            test_img = np.expand_dims(test_img, axis = 0)
+
+            """
+            import cv2
+            #image load with opencv
+            img = cv2.imread("deneme/visne.png")
+            img = cv2.resize(img, (224,224))
+            img = np.array(img, dtype="float32")
+            img = np.reshape(img, (1,224,224,3))
+            """
+
+            #Modelin yüklenmesi ve giriş çıkış tensorlerin hazırlanması
+            interpreter = tf.lite.Interpreter(model_path="yepyeni_kalite.tflite")
+            interpreter.allocate_tensors()
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+
+
+
+            #Giriş tensorü olarak diziye dönüştürülmüş imgenin verilmesi ve prediction işlemi
+            interpreter.set_tensor(input_details[0]['index'],test_img)
+            interpreter.invoke()
+            predictions = tf.math.softmax(interpreter.get_tensor(output_details[0]['index']))
+
+
+            print(predictions)
+
+
+
+            #olasılıklar arasından en büyüğünün seçilmesi ve tür eşleştirilmesi
+            max_index = np.argmax(predictions)
+
+            classes_kalite = ['Q4A','Q4B','Q3C','Q3B','Q3A','Q4C']
+
+            print(classes_kalite[max_index])
+
+            self.kalite_turu = str(classes_kalite[max_index])
+
+
+        except:
+
+            self.kalite_turu = "Kalite Türü Bulunamadı."
+
 
         return super().save(*args, **kwargs)
-
-        
-    
-
-
-
-
-        
-
-        
-    
-
-
--- marbleproject/marble/views.py
+++ marbleproject/marble/views.py
@@ -16,8 +16,12 @@ def anasayfa(request):
     son_resim = son_nesne.image.url
     ad = son_nesne.title
     renk = son_nesne.average_color
+    crack = son_nesne.crack
+    dot = son_nesne.dot
+    good = son_nesne.good
+    joint = son_nesne.joint
+    kalite_turu = son_nesne.kalite_turu
     
-
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES)
         
@@ -32,5 +36,5 @@ def anasayfa(request):
         form = ImageForm()
 
 
-    return render(request, "marble/anasayfa.html",{"images":son_resim, "form":form, "ad":ad, "renk":renk})
+    return render(request, "marble/anasayfa.html",{"images":son_resim, "form":form, "ad":ad, "renk":renk, "crack":crack, "dot":dot, "good":good, "joint":joint, "kalite_turu":kalite_turu})
 
